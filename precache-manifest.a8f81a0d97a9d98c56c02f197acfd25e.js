a405c1662691b4ae601ac9938cb7d4b8a622ee2e
unknown <quangnguyen5598@gmail.com>
2020-02-06 22:35:26 -0500

    Updates

---
+++ precache-manifest.a8f81a0d97a9d98c56c02f197acfd25e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2c842e2201366d87be24f76b04281c",
+    "url": "/quang5598/grammar-police/index.html"
+  },
+  {
+    "revision": "081982d64d9d6790c444",
+    "url": "/quang5598/grammar-police/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "109675b69c1c95f4676f",
+    "url": "/quang5598/grammar-police/static/css/main.95ad90e0.chunk.css"
+  },
+  {
+    "revision": "081982d64d9d6790c444",
+    "url": "/quang5598/grammar-police/static/js/2.915a023b.chunk.js"
+  },
+  {
+    "revision": "3d416322b0ec4a5a91d5be4afe7c848c",
+    "url": "/quang5598/grammar-police/static/js/2.915a023b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "109675b69c1c95f4676f",
+    "url": "/quang5598/grammar-police/static/js/main.41e6a256.chunk.js"
+  },
+  {
+    "revision": "f252076c3bc6d1fb92e0",
+    "url": "/quang5598/grammar-police/static/js/runtime-main.6dd7cbd8.js"
+  },
+  {
+    "revision": "69ddbe02d5d53b008825380e9bcaa29c",
+    "url": "/quang5598/grammar-police/static/media/icon.69ddbe02.png"
+  },
+  {
+    "revision": "28686cfad296c375d5363f9598fb0028",
+    "url": "/quang5598/grammar-police/static/media/image1.28686cfa.jpg"
+  },
+  {
+    "revision": "091a183052858fc02137193bb387840b",
+    "url": "/quang5598/grammar-police/static/media/image2.091a1830.jpg"
+  }
+]);
